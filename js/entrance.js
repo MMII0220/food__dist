4a4857a9f8d8bcb1f3fef66736f9750c10e5c370
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2023-05-18 08:03:19 +0300

    feat: add купшыекн

--- js/entrance.js
+++ js/entrance.js
@@ -3,6 +3,12 @@ window.addEventListener('DOMContentLoaded', () => {
   //   event.preventDefault();
   //   window.location.href = '../website.html'; // Замените ссылку на нужную
   // });
+  document.querySelector('.registry').addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href = './registry-page.html'; // Замените ссылку на нужную
+    console.log('asd');
+  });
+
   document.getElementById('login-form').addEventListener('submit', function (event) {
     event.preventDefault(); // Отменяем отправку формы по умолчанию
 

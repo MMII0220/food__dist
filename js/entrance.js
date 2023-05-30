66c8e983cf334a0cf9752b36282a1691ed04d47c
Deathstroke Lowell <deathstl@ga-i3.msk.21-school.ru>
2023-05-30 20:00:13 +0300

    fix: registry form

--- js/entrance.js
+++ js/entrance.js
@@ -1,20 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
-  // document.getElementById('login-form').addEventListener('submit', function (event) {
-  //   event.preventDefault();
-  //   window.location.href = '../website.html'; // Замените ссылку на нужную
-  // });
   document.querySelector('.registry').addEventListener('click', function (event) {
     event.preventDefault();
-    window.location.href = './registry-page.html'; // Замените ссылку на нужную
-    console.log('asd');
+    window.location.href = './registry-page.html';
   });
 
-  document.getElementById('login-form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Отменяем отправку формы по умолчанию
-
-    // Добавьте здесь код для обработки формы, проверки данных и отправки на сервер
-
-    // Перенаправление на другую страницу
-    window.location.href = './website.html'; // Замените ссылку на нужную
+  document.getElementById('enter-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    window.location.href = './website.html';
   });
+
+  document.getElementById('registry-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    window.location.href = './index.html';
+  });
+  document.querySelector('#registry-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    window.location.href = './index.html';
+  });  // Не работает
 });
